c923c9350b38398526116cc25c667f7285749ba8
Vadim Skidanov <vadim.skidanovs@gmail.com>
2020-12-21 20:09:00 +0000

    first js commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,56 @@
-console.log("Hey Dude!");
+const daysOfMonth = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  21,
+  22,
+  23,
+  24,
+  25,
+  26,
+  27,
+  28,
+  29,
+  30,
+  31,
+];
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+const displayDate = () => {
+  const classDate = document.querySelector(".date");
+  for (let day of daysOfMonth) {
+    classDate.innerHTML += `
+    <div class="date__day"> ${day}
+    `;
+  }
+};
+
+const displayDay = () => {
+  const parrentWeek = document.querySelector(".day-of-week");
+  for (let date of daysOfWeek) {
+    parrentWeek.innerHTML += `
+    <div>${date}</div>
+    `;
+  }
+};
+
+displayDay();
+displayDate();
