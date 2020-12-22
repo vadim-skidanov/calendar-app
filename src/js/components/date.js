98aa1bb415717ada6eefb535bae1cb18812b6164
Vadim Skidanov <vadim.skidanovs@gmail.com>
2020-12-22 11:42:36 +0000

    years in js

--- src/js/components/date.js
+++ src/js/components/date.js
@@ -1,1 +1,6 @@
-export const Date = (date) => `<div class="date__day">${date}</div>`;
+export const dateOfMonth = (date) =>
+  `<div class="date__day date__day-${date}">${date}</div>`;
+
+export const daysInMonth = (month, year) => {
+  return new Date(year, month, 0).getDate();
+};
