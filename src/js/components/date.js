9259aeef046551a0a2be0f8b6275db4c646c4fcf
Vadim Skidanov <vadim.skidanovs@gmail.com>
2020-12-28 20:42:41 +0000

    dates finish

--- src/js/components/date.js
+++ src/js/components/date.js
@@ -1,6 +1,12 @@
-export const dateOfMonth = (date) =>
+export { dateOfMonth, daysInMonth, startOfMonth };
+
+const dateOfMonth = (date) =>
   `<div class="date__day date__day-${date}">${date}</div>`;
 
-export const daysInMonth = (month, year) => {
+const daysInMonth = (month, year) => {
   return new Date(year, month, 0).getDate();
+};
+
+const startOfMonth = (date) => {
+  return new Date(date.getFullYear(), date.getMonth(), 1);
 };
