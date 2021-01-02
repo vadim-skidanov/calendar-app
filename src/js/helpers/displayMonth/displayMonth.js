bccff4c609b84b53be82cfd5cfe5121c6ab3e364
Vadim Skidanov <vadim.skidanovs@gmail.com>
2021-01-02 17:59:41 +0000

    refactor

--- src/js/helpers/displayMonth/displayMonth.js
+++ src/js/helpers/displayMonth/displayMonth.js
@@ -5,17 +5,19 @@ export const displayMonth = (currentMonth) => {
   const parentPrev = document.querySelector(".month__prev-name");
   const parentNext = document.querySelector(".month__next-name");
 
+  const switchBetweenMonths = (next, prev) => {
+    parentNext.textContent = shortMonths[currentMonth - next];
+    parentPrev.textContent = shortMonths[currentMonth - prev];
+  };
+
   const displayCurrent = () => {
     parentCurrent.textContent = months[currentMonth];
     if (currentMonth === 11) {
-      parentNext.textContent = shortMonths[currentMonth - 11];
-      parentPrev.textContent = shortMonths[currentMonth - 1];
+      switchBetweenMonths(11, 1);
     } else if (currentMonth === 0) {
-      parentNext.textContent = shortMonths[currentMonth + 1];
-      parentPrev.textContent = shortMonths[currentMonth + 11];
+      switchBetweenMonths(-1, -11);
     } else {
-      parentNext.textContent = shortMonths[currentMonth + 1];
-      parentPrev.textContent = shortMonths[currentMonth - 1];
+      switchBetweenMonths(-1, 1);
     }
   };
 
