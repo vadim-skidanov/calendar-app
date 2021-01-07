e6183c3c77abe34a57add0c858500eb6441092c2
Vadim Skidanov <vadim.skidanovs@gmail.com>
2021-01-07 00:28:24 +0000

    add events

--- src/js/helpers/displayEvent/displayEvent.js
+++ src/js/helpers/displayEvent/displayEvent.js
@@ -6,15 +6,52 @@ export const displayEvent = () => {
 
     for (let i = 0; i < dateParent.length; i++) {
       dateParent[i].addEventListener("click", () => {
-        let hello = popupParent;
-        hello.classList.add("event-popup--active");
-
+        popupParent.classList.add("event-popup--active");
         popupClose.addEventListener("click", () => {
-          hello.classList.remove("event-popup--active");
+          popupParent.classList.remove("event-popup--active");
         });
       });
     }
   };
 
+  const addAndDisplayEvents = () => {
+    const eventTime = document.querySelector("#time");
+    const eventTitle = document.querySelector("#title");
+    const eventContent = document.querySelector(".event");
+    const btnInsert = document.querySelector("#btnInsert");
+
+    btnInsert.addEventListener("click", () => {
+      const eTime = eventTime.value;
+      const eTitle = eventTitle.value;
+      if (eTime && eTitle) {
+        const storage = JSON.parse(localStorage.getItem("user"));
+        if (storage !== null && storage.length > 0) {
+          storage.push({
+            time: eTime,
+            title: eTitle,
+          });
+          localStorage.setItem("user", JSON.stringify(storage));
+        } else {
+          localStorage.setItem(
+            "user",
+            JSON.stringify([{ time: eTime, title: eTitle }])
+          );
+        }
+        location.reload();
+      }
+    });
+
+    const savedEvents = JSON.parse(localStorage.getItem("user"));
+    if (savedEvents !== null) {
+      let parentEvent = "";
+      for (let event of savedEvents) {
+        parentEvent += `<div class="event__time">${event.time}</div>
+            <div class="event__title">${event.title}</div>`;
+      }
+      eventContent.innerHTML = parentEvent;
+    }
+  };
+
   displayAndClosePopup();
+  addAndDisplayEvents();
 };
